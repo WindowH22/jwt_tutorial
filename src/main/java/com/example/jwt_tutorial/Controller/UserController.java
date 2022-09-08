efb70c6baeecdf47f8049a2b6aa8d87550829302
Chang1235 <dlckdgml1235@naver.com>
2022-09-08 16:51:47 +0900

    feat: Add UserController

--- src/main/java/com/example/jwt_tutorial/Controller/UserController.java
+++ src/main/java/com/example/jwt_tutorial/Controller/UserController.java
@@ -1,4 +1,39 @@
 package com.example.jwt_tutorial.Controller;
 
+import com.example.jwt_tutorial.dto.UserDto;
+import com.example.jwt_tutorial.entity.User;
+import com.example.jwt_tutorial.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(
+            @Valid @RequestBody UserDto userDto
+    ){
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')") // USER,ADMIN 두가지 권한 혀용
+    public ResponseEntity<User> getMyUserInfo(){
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // ADMIN 두가지 권한 혀용
+    public ResponseEntity<User> getUserInfo(@PathVariable String username){
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+
+
 }
